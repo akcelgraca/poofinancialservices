e0d23fdf25f81a8f3eb74c31a10cfcb008c52afd
Akcel Soares da Graça <132278847+akcelgraca@users.noreply.github.com>
2024-11-15 17:07:46 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cliente.java
    #	src/Produto.java

--- src/Produtoalimentar.java
+++ src/Produtoalimentar.java
@@ -1,3 +1,12 @@
 public class Produtoalimentar extends Produto {
+    private boolean biologico;
+    public Produtoalimentar(String codigo, String nome, String descricao,int quantidade,double valorUnitario){
+        super(codigo,nome,descricao,quantidade,valorUnitario);
+        this.biologico = biologico;
+    }
 
+    @Override
+    public double calcularIVA() {
+        return 0;
+    }
 }
--- src/Produtofarmacia.java
+++ src/Produtofarmacia.java
@@ -1,2 +1,11 @@
 public class Produtofarmacia extends Produto {
+    public Produtofarmacia(String codigo, String nome, String descricao,int quantidade,double valorUnitario){
+        super(codigo,nome,descricao,quantidade,valorUnitario);
+
+    }
+    @Override
+    public double calcularIVA() {
+        return 0;
+    }
 }
+
